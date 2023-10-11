d304fff099277488de97813c1fbf686b89c9c359
Mohammad Aditya Baihaqi <144421054+AdityaBaihaqi@users.noreply.github.com>
2023-10-11 16:13:16 +0700

    Add files via upload

---
+++ Pertemuan 3/script.js
@@ -0,0 +1,7 @@
+let kebenaran = prompt("Masukkan 2 jika benar, masukkan 0 jika salah")
+
+let cek = confirm("apakah sudah benar?")
+
+let hasil = kebenaran == "2" ? "Benar" : "Salah"
+
+alert(hasil)
